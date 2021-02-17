e7caca9f0290f25b843527f4e91592e0390b9bfc
Manuel Faria <mfaria724@gmail.com>
2021-02-17 11:26:48 +0000

    Add zipWith implementation.

---
+++ pregunta4/zip.py
@@ -0,0 +1,11 @@
+# implementation of zipWith 
+# a and b should be lists and func should be a function
+def zipWith(a, b, func):
+  if a and b:
+    yield func(a[0], b[0])
+    for p in zipWith(a[1:], b[1:], func):
+      yield p
+
+# execution example
+for p in zipWith([0,1,2,1], [1,2,1,0], lambda x, y: x + y):
+  print(p)
